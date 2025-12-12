25d84b5763aa14f960806f8192c33cc0497cae0e
ask4fusora <ask4fusora+github@gmail.com>
2025-12-12 17:36:12 +0700

    Fix casing typo

--- Pawtopia/Data/PawtopiaDbContext.cs
+++ Pawtopia/Data/PawtopiaDbContext.cs
@@ -57,7 +57,7 @@ namespace Pawtopia.Data
                     Id = customerRoleId,
                     ConcurrencyStamp = customerRoleId,
                     Name = "Customer",
-                    NormalizedName = "Customer"
+                    NormalizedName = "CUSTOMER"
                 }
             );
         }
