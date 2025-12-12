252eb6964f3d394b84944da10d275ce348317dbb
ask4fusora <ask4fusora+github@gmail.com>
2025-12-12 12:59:58 +0700

    Use `tailwindcss`

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+export const content = [
+  "./Pawtopia/**/*.razor",
+  "./Pawtopia/**/*.html",
+  "./Pawtopia.Client/**/*.razor",
+  "./Pawtopia.Client/**/*.html"
+];
+export const theme = {
+  extend: {},
+};
+export const plugins = [];
